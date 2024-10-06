d3aa43060ecddb2958a7ee2b795ffbc4f5b9d9b4
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-10-06 19:17:57 +0100

    registry

--- autumn-core/src/test/java/autumn/core/test/discovery/MulticastDiscoveryTest.java
+++ autumn-core/src/test/java/autumn/core/test/discovery/MulticastDiscoveryTest.java
@@ -1,7 +1,11 @@
 package autumn.core.test.discovery;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
@@ -52,6 +56,95 @@ public class MulticastDiscoveryTest {
             System.in.read();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testSender() {
+        MulticastSocket sendSocket = null;
+        try {
+            sendSocket = new MulticastSocket();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(new Send2(sendSocket)).start();
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testReceive() {
+        MulticastSocket receSocket = null;
+        try {
+            receSocket = new MulticastSocket(6789);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        new Thread(new Rece2(receSocket)).start();
+        log.info("begin receive listening");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class Rece2 implements Runnable {
+        private  MulticastSocket socket;
+        public Rece2(MulticastSocket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                byte[] buff = new byte[1024];
+                InetAddress group = InetAddress.getByName("228.5.6.7");
+                socket.joinGroup(group);
+                while(true) {
+                    DatagramPacket packet = new DatagramPacket(buff,buff.length);
+                    socket.receive(packet);
+                    String data = new String(packet.getData(),0,packet.getLength());
+                    String ip = packet.getAddress().getHostAddress();
+                    if("bye".equals(data)) {
+                        log.info("ip:{}, away", ip);
+                        continue;
+                    }
+                    log.info("ip: {}, say: {}", ip, data);
+                }
+            } catch (IOException e) {
+                log.warn("receive fail exception: {}", e);
+                throw new RuntimeException("receive fail!");
+            }
+        }
+    }
+
+    class Send2 implements Runnable {
+        private MulticastSocket socket;
+        public Send2(MulticastSocket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                InetAddress group = InetAddress.getByName("228.5.6.7");
+                socket.joinGroup(group);
+                String line = null;
+                while((line = reader.readLine())!= null) {
+                    byte[] buff = line.getBytes();
+                    DatagramPacket packet = new DatagramPacket(buff,buff.length,group,6789);
+                    socket.send(packet);
+                    if("bye".equals(line)) {
+                        break;
+                    }
+                }
+                socket.close();
+            } catch (IOException e) {
+                throw new RuntimeException("发送端失败");
+            }
         }
     }
 
