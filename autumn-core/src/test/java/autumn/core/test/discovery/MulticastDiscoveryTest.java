670e42447befe2f87d3f8c04913f27dfaed852d9
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-10-07 02:55:06 +0100

    registry

--- autumn-core/src/test/java/autumn/core/test/discovery/MulticastDiscoveryTest.java
+++ autumn-core/src/test/java/autumn/core/test/discovery/MulticastDiscoveryTest.java
@@ -8,10 +8,13 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 
 import autumn.core.config.ApplicationConfig;
 import autumn.core.config.ProviderConfig;
@@ -29,7 +32,7 @@ public class MulticastDiscoveryTest {
     @Test
     void testNetworkInterface() {
         try {
-            Enumeration<NetworkInterface> interfaces =  NetworkInterface.getNetworkInterfaces();
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
             while (interfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = interfaces.nextElement();
                 Enumeration<InetAddress> inetAddressEnumeration = networkInterface.getInetAddresses();
@@ -93,22 +96,24 @@ public class MulticastDiscoveryTest {
     }
 
     class Rece2 implements Runnable {
-        private  MulticastSocket socket;
+        private MulticastSocket socket;
+
         public Rece2(MulticastSocket socket) {
             this.socket = socket;
         }
+
         @Override
         public void run() {
             try {
                 byte[] buff = new byte[1024];
                 InetAddress group = InetAddress.getByName("228.5.6.7");
                 socket.joinGroup(group);
-                while(true) {
-                    DatagramPacket packet = new DatagramPacket(buff,buff.length);
+                while (true) {
+                    DatagramPacket packet = new DatagramPacket(buff, buff.length);
                     socket.receive(packet);
-                    String data = new String(packet.getData(),0,packet.getLength());
+                    String data = new String(packet.getData(), 0, packet.getLength());
                     String ip = packet.getAddress().getHostAddress();
-                    if("bye".equals(data)) {
+                    if ("bye".equals(data)) {
                         log.info("ip:{}, away", ip);
                         continue;
                     }
@@ -123,27 +128,103 @@ public class MulticastDiscoveryTest {
 
     class Send2 implements Runnable {
         private MulticastSocket socket;
+
         public Send2(MulticastSocket socket) {
             this.socket = socket;
         }
+
         @Override
         public void run() {
             try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
                 InetAddress group = InetAddress.getByName("228.5.6.7");
                 socket.joinGroup(group);
-                String line = null;
-                while((line = reader.readLine())!= null) {
+                String line = "Hello: ".concat(UUID.randomUUID().toString());
+                while (true) {
                     byte[] buff = line.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buff,buff.length,group,6789);
+                    DatagramPacket packet = new DatagramPacket(buff, buff.length, group, 30881);
+                    log.info("send message:{}", line);
                     socket.send(packet);
-                    if("bye".equals(line)) {
+                    Thread.sleep(1000L);
+                    if ("bye".equals(line)) {
                         break;
                     }
                 }
                 socket.close();
             } catch (IOException e) {
                 throw new RuntimeException("发送端失败");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Test
+    void test3() {
+        InetAddress group = null;
+        try {
+            group = InetAddress.getByName("224.0.0.1");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        int port = 5555;
+        MulticastSocket ms = null;
+
+        try {
+            ms = new MulticastSocket(port);
+            ms.joinGroup(group);//加入到组播组
+            while (true) {
+                String message = "Hello " + new java.util.Date();
+                byte[] buffer = message.getBytes();
+                DatagramPacket dp = new DatagramPacket(buffer, buffer.length, group, port);
+                ms.send(dp);//发送组播数据报
+                System.out.println("发送数据报给" + group + ":" + port);
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (ms != null) {
+                try {
+                    ms.leaveGroup(group);
+                    ms.close();
+                } catch (IOException e) {
+                    log.warn("exception: ", e);
+                }
+
+            }
+        }
+    }
+
+    @Test
+    void test4() {
+        InetAddress group = null;
+        try {
+            group = InetAddress.getByName("224.0.0.1");
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        int port = 5555;
+        MulticastSocket ms = null;
+        try {
+            ms = new MulticastSocket(port);
+            ms.joinGroup(group);
+            byte[] buffer = new byte[8192];
+            while (true) {
+                DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+                ms.receive(dp);
+                String s = new String(dp.getData(), 0, dp.getLength());
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ms != null) {
+                try {
+                    ms.leaveGroup(group);
+                    ms.close();
+                } catch (IOException e) {
+                    log.warn("exception: ", e);
+                }
             }
         }
     }
